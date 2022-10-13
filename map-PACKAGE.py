ee495fe09d256b97c00656636d84f56361cbd6ae
fjf64 <56519341+fjf64@users.noreply.github.com>
2022-10-13 15:04:21 -0400

    added path stretch
    paths feeling too organic? Try path stretching, allowing long corridors to be generated easily at any length.

--- map-PACKAGE.py
+++ map-PACKAGE.py
@@ -1,6 +1,7 @@
 import random as r
 if True:
     # A small package + a starter line or two, that is used for making maps and visuals.
+    #To-Do :: 
     """ map coords
         #ABCD
         1####
@@ -9,11 +10,10 @@ if True:
         4####
     """
     areamap = {}
-    pathgen_parameters = 50
     walker_pos_list = []
-    mapsize = [5, 10] #!!! IMPORTANT !!!#
+    mapsize = [5, 20] #!!! IMPORTANT !!!#
     mapmid = [round(mapsize[0] / 2), round(mapsize[1] / 2)]
-    oldpp = [3, 5] # INITIAL PLAYER POSITION
+    oldpp = [3, 10] # INITIAL PLAYER POSITION
     origin = oldpp
     newpp = oldpp
     player = "X"
@@ -98,28 +98,31 @@ if True:
                 print("Out of Bounds")
             editmap(newpp[0], newpp[1], player, True)
             printmap() # Map Package - minimize
-    def pathgen(path_length):
+    def pathgen(path_length,path_stretch = 1):
         global walker_pos_list
-        """creates randomized paths using [walkable] variable as the path icon"""
+        """creates randomized paths using [walkable] variable as the path, and path stretch is the linear distance covered before changing direction. """
         oldpp = origin
         random_walkers = [oldpp[0], oldpp[1]]
         walker_pos_list = [[oldpp[0], oldpp[1]]]
         while path_length > 0:
             walker_direction = r.randint(1,2)
-            if walker_direction == 1:
-                random_walkers[0] += r.choice([-1, 1])
-            if walker_direction == 2:
-                random_walkers[1] += r.choice([-1, 1])
-            if random_walkers[0] <= mapsize[0] and random_walkers[1] <= mapsize[1] and random_walkers[0] >= 1 and random_walkers[1] >= 1:
-                walker_pos_list.append([random_walkers[0], random_walkers[1]])
-                path_length -= 1
-            else:
-                random_walkers[0] = walker_pos_list[0][0]
-                random_walkers[1] = walker_pos_list[0][1]
-        for x in range(len(walker_pos_list)):
-            editmap(walker_pos_list[x][0], walker_pos_list[x][1], walkable)
+            walker_speed = r.choice([-1, 1])
+            for x in range(path_stretch):
+                if walker_direction == 1:
+                    random_walkers[0] += walker_speed
+                if walker_direction == 2:
+                    random_walkers[1] += walker_speed
+                if random_walkers[0] <= mapsize[0] and random_walkers[1] <= mapsize[1] and random_walkers[0] >= 1 and random_walkers[1] >= 1:
+                    if not random_walkers in walker_pos_list:
+                        walker_pos_list.append([random_walkers[0], random_walkers[1]])
+                        path_length -= 1
+                else:
+                    random_walkers[0] = walker_pos_list[0][0]
+                    random_walkers[1] = walker_pos_list[0][1]
+            for x in range(len(walker_pos_list)):
+                editmap(walker_pos_list[x][0], walker_pos_list[x][1], walkable)
 makemap(mapsize[0],mapsize[1], blank)
-pathgen(pathgen_parameters)
+pathgen(50, 2)
 editmap(oldpp[0], oldpp[1], player, True)
 printmap()
 while True:
