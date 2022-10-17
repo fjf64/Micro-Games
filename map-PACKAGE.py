1964d0bce413ac47efc37f90c59004adaf6c08db
fjf64 <56519341+fjf64@users.noreply.github.com>
2022-10-17 11:21:28 -0400

    added direction bias

--- map-PACKAGE.py
+++ map-PACKAGE.py
@@ -9,11 +9,11 @@ if True:
         3####
         4####
     """
-    areamap = {}
+    areamap = {} # if a custom map was made, insert here
     walker_pos_list = []
-    mapsize = [5, 20] #!!! IMPORTANT !!!#
+    mapsize = [5, 100] #!!! IMPORTANT !!!#
     mapmid = [round(mapsize[0] / 2), round(mapsize[1] / 2)]
-    oldpp = [3, 10] # INITIAL PLAYER POSITION
+    oldpp = [3, 50] # INITIAL PLAYER POSITION
     origin = oldpp
     newpp = oldpp
     player = "X"
@@ -98,14 +98,22 @@ if True:
                 print("Out of Bounds")
             editmap(newpp[0], newpp[1], player, True)
             printmap() # Map Package - minimize
-    def pathgen(path_length,path_stretch = 1):
+    def pathgen(path_length,path_stretch = 1, path_bias_x = 0, path_bias_y = 0):
         global walker_pos_list
-        """creates randomized paths using [walkable] variable as the path, and path stretch is the linear distance covered before changing direction. """
+        """creates randomized paths using [walkable] variable as the path, and path stretch is the linear distance covered before changing direction.  path bias is if the path leans towards verticality or horizontal movement, with a higher or lower ratio changing the amount of times it re-rolls the direction"""
         oldpp = origin
         random_walkers = [oldpp[0], oldpp[1]]
         walker_pos_list = [[oldpp[0], oldpp[1]]]
         while path_length > 0:
+            cwbiasx = path_bias_x
+            cwbiasy = path_bias_y
             walker_direction = r.randint(1,2)
+            while walker_direction != 2 and cwbiasx > 0:
+                walker_direction = r.randint(1,2)
+                cwbiasx -= 1
+            while walker_direction != 1 and cwbiasy > 0:
+                walker_direction = r.randint(1,2)
+                cwbiasy -= 1
             walker_speed = r.choice([-1, 1])
             for x in range(path_stretch):
                 if walker_direction == 1:
@@ -122,7 +130,7 @@ if True:
             for x in range(len(walker_pos_list)):
                 editmap(walker_pos_list[x][0], walker_pos_list[x][1], walkable)
 makemap(mapsize[0],mapsize[1], blank)
-pathgen(50, 2)
+pathgen(100, 2, 3)
 editmap(oldpp[0], oldpp[1], player, True)
 printmap()
 while True:
